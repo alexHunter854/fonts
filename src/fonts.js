95ea0cc47843f16c9b8a5b7bb030898ef22db6f9
alexHunter854 <61906467+alexHunter854@users.noreply.github.com>
2021-01-06 10:16:57 +0200

    Create fonts.js

---
+++ src/fonts.js
@@ -0,0 +1,25 @@
+function fonts(){
+  this.arial = "font-family: Arial"
+  this.verdana  = "font-family: Verdana"
+  this.helvetica = "font-family: Helvetica"
+  this.tahoma = "font-family: Tahoma"
+  this.trebuchet = "font-family: Trebuchet-MS"
+  this.roman = "font-family: Times-New-Roman"
+  this.georgia = "font-family: Georgia"
+  this.garamond = "font-family: Garamond"
+  this.typeWriter = "font-family: Courier-New"
+  this.brush_Script = "font-family: Brush-Script-MT"
+  this.options = function(){
+    console.log("options:")
+    console.log("arial")
+    console.log("verdana")
+    console.log("helvetica")
+    console.log("tahoma")
+    console.log("trebuchet")
+    console.log("roman")
+    console.log("georgia")
+    console.log("garamond")
+    console.log("typeWriter")
+    console.log("brush_Script")
+  }
+}
